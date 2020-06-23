2d3c6c7cace4c86251e3a72675b374ba004bd494
Mandisa Reed <mandisa.reed@gmail.com>
2020-06-22 22:56:13 -0400

    created container in which information values are to be stored

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 var apiKey = "7e9791df05c08ac90ec357d00e62c2f9";
 
-$("submit-city-btn").on("click", function () {
+$("#submit-city-btn").on("click", function() {
     //not sure if I need to prevent default yet...
     //event.preventDefault;
-    var city = $("#city-input").val();
+    var city = $(".city-input").val();
     var queryURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" 
     + city 
     + "&appid=" 
     + apiKey;
+    alert("button has been clicked");
+
+    $.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function (response) {
+    var results = response.data;
+    console.log(queryURL);
+    console.log(response);
+
+});
+
+
 });
