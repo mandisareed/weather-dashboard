19a68db376eb14684ce87a4d9aaceef2fb73cf53
Mandisa Reed <mandisa.reed@gmail.com>
2020-06-25 01:36:17 -0400

    created div to display current weather, added UV Index, and cleared content of forecast so it wouldn't continuously append each city search

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
-
   $("#search-button").on("click", function () {
-
-  var apiKey = "7e9791df05c08ac90ec357d00e62c2f9";
-  var city = $("#city-input").val();
+    var apiKey = "7e9791df05c08ac90ec357d00e62c2f9";
+    var city = $("#city-input").val();
 
     var queryURL =
       "https://api.openweathermap.org/data/2.5/forecast?q=" +
@@ -18,9 +16,6 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log(response);
 
-      var testDate = new Date(1593021600*1000);
-      console.log(testDate);
-
       var forecastDays = [];
       forecastDays.push(response.list[2]);
       forecastDays.push(response.list[10]);
@@ -28,27 +23,76 @@ $(document).ready(function () {
       forecastDays.push(response.list[26]);
       forecastDays.push(response.list[34]);
 
-      //pull data needed to display in html div
-      //loop through it
-      for (var i = 0; i < forecastDays.length; i ++) { 
-      var dataContainer = $("<div>").addClass("forecast-data-container");
+      var lat = response.city.coord.lat;
+      var long = response.city.coord.lon;
+      var uvUrl =
+        "http://api.openweathermap.org/data/2.5/uvi?appid=" +
+        apiKey +
+        "&lat=" +
+        lat +
+        "&lon=" +
+        long;
+      //create div in which uv index will be stored BEFORE the request for its data/value, so that the reponse has a home. otherwise, the browser will try to update the div before having the data.
+      var uvEl = $("<div>");
+      $.ajax({
+        url: uvUrl,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
+        uvEl.text("UV Index: " + JSON.stringify(response.value));
+      });
+
       var cityNameEl = $("<div>").text(response.city.name);
-      var dateEL = $("<div>").text(forecastDays[i].dt_txt);
-      var iconID = forecastDays[i].weather[0].icon;
-      var weatherIcon = "http://openweathermap.org/img/wn/" + iconID + "@2x.png";
-      var iconEl = $("<div>").prepend("<img src='"+ weatherIcon + "' alt='Weather Icon' />")
-      var tempEl = $("<div>").text("Temperature: " + forecastDays[i].main.temp + " °F");
-      var humidityEl = $("<div>").text("Humidity: " + forecastDays[i].main.humidity);
-      //var windspeedEL = $("<div>").text("Wind Speed: " + forecastDays[i].wind.speed + " mph");
-      //create a var for UV index (will need long + lat). will prob need to create another URL query
+      var dateEL = $("<div>").text(forecastDays[0].dt_txt);
+      var iconID = forecastDays[0].weather[0].icon;
+      var weatherIcon =
+        "http://openweathermap.org/img/wn/" + iconID + "@2x.png";
+      var iconEl = $("<div>").prepend(
+        "<img src='" + weatherIcon + "' alt='Weather Icon' />"
+      );
+      var tempEl = $("<div>").text(
+        "Temperature: " + forecastDays[0].main.temp + " °F"
+      );
+      var humidityEl = $("<div>").text(
+        "Humidity: " + forecastDays[0].main.humidity
+      );
+      var windspeedEL = $("<div>").text(
+        "Wind Speed: " + forecastDays[0].wind.speed + " mph"
+      );
+      var currentContainer = $("<div>").addClass("current-container");
 
-      
-      dataContainer.append(cityNameEl, dateEL, iconEl, tempEl, humidityEl);
-      //append the data Container to the html
-      $("#forecast-data-display").append(dataContainer);
+      currentContainer.append(
+        cityNameEl,
+        dateEL,
+        iconEl,
+        tempEl,
+        humidityEl,
+        windspeedEL,
+        uvEl
+      );
+      $("#current").empty();
+      $("#current").append(currentContainer);
 
-    
+      $("#forecast-data-display").empty();
+      for (var i = 0; i < forecastDays.length; i++) {
+        var dataContainer = $("<div>").addClass("forecast-data-container");
+        var cityNameEl = $("<div>").text(response.city.name);
+        var dateEL = $("<div>").text(forecastDays[i].dt_txt);
+        var iconID = forecastDays[i].weather[0].icon;
+        var weatherIcon =
+          "http://openweathermap.org/img/wn/" + iconID + "@2x.png";
+        var iconEl = $("<div>").prepend(
+          "<img src='" + weatherIcon + "' alt='Weather Icon' />"
+        );
+        var tempEl = $("<div>").text(
+          "Temperature: " + forecastDays[i].main.temp + " °F"
+        );
+        var humidityEl = $("<div>").text(
+          "Humidity: " + forecastDays[i].main.humidity
+        );
 
+        dataContainer.append(cityNameEl, dateEL, iconEl, tempEl, humidityEl);
+        $("#forecast-data-display").append(dataContainer);
       }
     });
   });
